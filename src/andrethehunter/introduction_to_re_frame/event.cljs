f145b32428b416b56cd7c76cc812fb10857df1c8
André de Jager <andrethehunter@gmail.com>
2018-07-03 21:18:56 +1000

    Hash addressing

--- src/andrethehunter/introduction_to_re_frame/event.cljs
+++ src/andrethehunter/introduction_to_re_frame/event.cljs
@@ -1,6 +1,8 @@
 (ns andrethehunter.introduction-to-re-frame.event
   (:require
     [andrethehunter.introduction-to-re-frame.slides :as slides]
+    [clojure.string :as string]
+    [oops.core :refer [oget oset! ocall]]
     [re-frame.core :as rf]))
 
 (def ^:private interceptors [(when goog.DEBUG rf/debug) rf/trim-v])
@@ -14,12 +16,34 @@
                         (conj interceptors (rf/path path)))]
      (rf/reg-event-db id interceptors handler))))
 
+(defn- location-hash
+  ([]
+   (re-find #"(?<=#).+" (oget js/location "hash")))
+  ([hash]
+   (oset! js/location "hash" hash)))
+
+(defn- hash->position [db]
+  (let [position (let [hash (location-hash)]
+                   (if (seq hash)
+                     (mapv js/Number (string/split hash "-"))
+                     [0 0]))]
+    (assoc db
+      :position position)))
+
 (reg-event ::initialize
-  (constantly {:position [0 0]
-               :slides   slides/-order}))
+  (fn [db]
+    (ocall js/window "addEventListener" "hashchange" (fn [] (rf/dispatch [::hash->position])) false)
+    (-> db
+        (assoc :slides slides/-order)
+        hash->position)))
 
 (reg-event ::update [:slides]
   (constantly slides/-order))
+
+(defn- set-position [db position]
+  (location-hash (string/join \- position))
+  (assoc db
+    :position position))
 
 (reg-event ::next
   (fn [{:keys                 [:slides]
@@ -33,8 +57,7 @@
           position (if (< max-points next-point)
                      [next-slide 0]
                      [slide-pos next-point])]
-      (assoc db
-        :position position))))
+      (set-position db position))))
 
 (reg-event ::prev
   (fn [{:keys                 [:slides]
@@ -48,5 +71,6 @@
           position (if (< prev-point min-points)
                      [prev-slide (count (:points (get slides prev-slide)))]
                      [slide-pos prev-point])]
-      (assoc db
-        :position position))))
+      (set-position db position))))
+
+(reg-event ::hash->position hash->position)
